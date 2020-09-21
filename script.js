03aeb849a1b79594c83888bd3f2a05f4f7b6dad7
vadymok <64975840+vadymok@users.noreply.github.com>
2020-09-21 16:31:57 +0200

    Update

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+/*
 setTimeout( function(){
 $(".param-filter-single.otevreny").closest("#category-filter-hover").addClass("vyfiltrovano");
 $("#category-filter-hover:not(.vyfiltrovano) .param-filter-single:first-child").addClass("otevreny");
-/*
-$('#category-filter-hover').each(function(){
-   $(this).css('min-height', $('.filter-section.otevreny form', this).outerHeight(true));
-});
-*/
+
 }, 1000);
+*/
 
 function mbTabfiltr(){
 if ( $('body.type-category').length ) {
@@ -14,26 +12,58 @@ if ( $('body.type-category').length ) {
 $(".filter-section > h4").click( function() {
 $(".filter-section").removeClass("otevreny");
 $(this).closest('.filter-section').addClass('otevreny');
+  
+  
+   
+   $("#filters > .slider-wrapper.filter-section-default").wrap('<div class="top-filters">');
+   $(".param-filter-top").prependTo(".top-filters");
+   //$(".top-filters").append('<button id="showFiltersData" class="ShowData"><span>Zobrazit značky a parametry</span></button>');
+   $('.param-filter-top .filter-label[for="stock"]').text('Na Skladě');
 
 $('#category-filter-hover').each(function(){
-   $(this).css('min-height', $('.filter-section.otevreny form', this).outerHeight(true));
+   $(this).css('min-height', $('.filter-section.otevreny form fieldset', this).outerHeight() + 70);
 });
 
 });
-$('#category-filter-hover .filter-section').click(function () {
+$('#category-filter-hover .filter-section:not(:last-child)').click(function () {
         $("#category-filter-hover .filter-section").removeClass('otevreny');
         $(this).addClass('otevreny');
         var activeIndex = $(this).index() + 1;
         localStorage.setItem('mySelectValue', activeIndex);
     });
 var activeIndex = localStorage.getItem('mySelectValue');
-    if (isNaN(activeIndex)) {} else {
-        $('#category-filter-hover .filter-section:nth-child('+activeIndex+')').addClass('otevreny');
+    if (isNaN(activeIndex) || localStorage.getItem('activeIndex') === null ) {
+      
+        localStorage.setItem('mySelectValue', '1');
+        $('#category-filter-hover .filter-section:nth-child(1)').addClass('otevreny');
+        //$('#category-filter-hover .filter-section:nth-child('+activeIndex+')').addClass('otevreny');
+        $("#filters > .slider-wrapper.filter-section-default").wrap('<div class="top-filters">');
+        $(".param-filter-top").prependTo(".top-filters");
+        $(".top-filters").append('<button id="showFiltersData" class="ShowData"><span>Zobrazit značky a parametry</span></button>');
+        $('.param-filter-top .filter-label[for="stock"]').text('Na Skladě');
+
+    } else {
+        $('#category-filter-hover .filter-section:nth-child('+activeIndex+')').addClass('otevreny'); /*ZDE Hazí chybu při spuštění scriptu pokud Tab ve filtru nebyl prokliknutý*/
+        //$('#showFiltersData').toggleClass('active');
+        //$('#filters > .filter-sections').toggle();
+        $('#category-filter-hover').each(function(){
+          $(this).css('min-height', $('.filter-section.otevreny form fieldset', this).outerHeight() + 70);
+        });
     }
 
-$("#stock-filter label").prependTo(".boolean-filter .parametric-filter").addClass("sklad");
 
 
+
+$('#showFiltersData').click(function () {
+
+  $(this).toggleClass('active');
+  $('#filters > .filter-sections').toggle();
+  
+  $('#category-filter-hover').each(function(){
+   $(this).css('min-height', $('.filter-section.otevreny form fieldset', this).outerHeight() + 70);
+  });
+   
+});
 
 
 if($('#clear-filters .filter-section-cell.active').length == 0) {
@@ -41,9 +71,7 @@ $(".filter-section-cell.active").parent().closest(".filter-section").clone().pre
 }
 
 /*
-
 Aktivní filtry
-
 */
 
 if ($(".filter-label").hasClass("active")) {
@@ -88,3 +116,20 @@ mbTabfiltr();
 $(document).ajaxSuccess(function(){
 mbTabfiltr();
 });
+
+
+/*
+Chyba, která byla na řádku 45 byla vyřešená tím, že se po načtení scriptu kontroluje jestli Localstorage obsahuje hodnotu, 
+pokud ne tak přidá defaultně hodnota do localstorage "1" 
+
+  * Přidat funnkci, která bude kontrolovat jestli byl filtr po AJAX Complete otevřený nebo zařený.
+    (Z důvodu, pokud si někdo při zavřeném filtru vybere jen - Filtrování dle ceny. V opačném případě je vadí to, 
+    že po Ajax dotazu se filtr vždy skryje)
+
+  * Řádek 39 - vzniká chyba pokud LoiscalStorage is empty (nepomáhá přidání hodnoty do LocalStorage na řádku 37)
+
+*/
+
+
+
+
